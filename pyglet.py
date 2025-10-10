bdc0cb896366620e10026af689c716108c3fd599
cells-OSS <open-source-version@proton.me>
2025-10-10 16:57:17 +0300

    Uncommented the code for the 'future release'

--- pyglet.py
+++ pyglet.py
@@ -5,50 +5,42 @@ import requests
 from packaging import version
 import os
 
-# __version__ = "v1.0"
-#
-#
-# def get_latest_release_tag():
-#    try:
-#        url = "https://api.github.com/repos/cells-OSS/pyglet/releases/latest"
-#        response = requests.get(url, timeout=5)
-#        response.raise_for_status()
-#        data = response.json()
-#        return data["tag_name"].lstrip("v")
-#    except Exception as e:
-#        print("Failed to check for updates:", e)
-#        return __version__.lstrip("v")
-#
-#
-# def is_update_available(current_version):
-#    latest = get_latest_release_tag()
-#    return version.parse(latest) > version.parse(current_version.lstrip("v"))
-#
-#
-# def download_latest_script():
-#    latest_version = get_latest_release_tag()
-#    filename = f"pyglet-v{latest_version}.py"
-#    url = "https://raw.githubusercontent.com/cells-OSS/pyglet/main/pyglet.py"
-#    response = requests.get(url)
-#    lines = response.text.splitlines()
-#    with open(filename, "w", encoding="utf-8") as f:
-#        for line in lines:
-#            f.write(line.rstrip() + "\n")
-#    print(
-#        f"Current version: {__version__}, Latest: v{get_latest_release_tag()}")
-#    print(
-#        f"Downloaded update as '{filename}'. You can now safely delete the old version.")
-#
-#    input("Press Enter to exit...")
-#    exit()
-#
-# if os.path.exists("auto_update.conf"):
-#    with open("auto_update.conf", "rb") as auto_update_configFile:
-#        auto_update_config = auto_update_configFile.read().decode()
-#        if auto_update_config == "True":
-#            if is_update_available(__version__):
-#                print("New version available!")
-#                download_latest_script()
+__version__ = "v1.0"
+def get_latest_release_tag():
+   try:
+       url = "https://api.github.com/repos/cells-OSS/pyglet/releases/latest"
+       response = requests.get(url, timeout=5)
+       response.raise_for_status()
+       data = response.json()
+       return data["tag_name"].lstrip("v")
+   except Exception as e:
+       print("Failed to check for updates:", e)
+       return __version__.lstrip("v")
+def is_update_available(current_version):
+   latest = get_latest_release_tag()
+   return version.parse(latest) > version.parse(current_version.lstrip("v"))
+def download_latest_script():
+   latest_version = get_latest_release_tag()
+   filename = f"pyglet-v{latest_version}.py"
+   url = "https://raw.githubusercontent.com/cells-OSS/pyglet/main/pyglet.py"
+   response = requests.get(url)
+   lines = response.text.splitlines()
+   with open(filename, "w", encoding="utf-8") as f:
+       for line in lines:
+           f.write(line.rstrip() + "\n")
+   print(
+       f"Current version: {__version__}, Latest: v{get_latest_release_tag()}")
+   print(
+       f"Downloaded update as '{filename}'. You can now safely delete the old version.")
+   input("Press Enter to exit...")
+   exit()
+if os.path.exists("auto_update.conf"):
+   with open("auto_update.conf", "rb") as auto_update_configFile:
+       auto_update_config = auto_update_configFile.read().decode()
+       if auto_update_config == "True":
+           if is_update_available(__version__):
+               print("New version available!")
+               download_latest_script()
 
 welcomeMessage = pyfiglet.figlet_format("Pyglet")
 
@@ -74,54 +66,36 @@ if chooseOption == 1:
 
         print(figlet_ver)
 
-# if chooseOption == 2:
-#
-#    settingsMenu = """
-# 1 = Change Auto-update settings
-# """
-#    print(settingsMenu)
-#    chooseSetting = int(input("Which option would you like to choose(1)?: "))
-#
-#    if chooseSetting == 1:
-#        aUpdateMenu = """
-#    ===============AUTO-UPDATE===============
-#
-#    1 = Turn on
-#    2 = Turn off
-#    """
-#
-#        print(aUpdateMenu)
-#        aUpdateOption = input(
-#            "Which option would you like to choose(1/2)?: ")
-#
-#        if aUpdateOption.lower() == "back":
-#            subprocess.Popen([sys.executable] + sys.argv)
-#            sys.exit()
-#
-#        if aUpdateOption == "1":
-#            with open("auto_update.conf", "wb") as auto_update_configFile:
-#                auto_update_configFile.write("True".encode())
-#
-#                print("Changes saved successfully!")
-#                input("Press any key to restart...")
-#                subprocess.Popen([sys.executable] + sys.argv)
-#                sys.exit()
-#
-#        if aUpdateOption == "2":
-#            with open("auto_update.conf", "wb") as auto_update_configFile:
-#                auto_update_configFile.write("False".encode())
-#
-#                print("Changes saved successfully!")
-#                input("Press any key to restart...")
-#                subprocess.Popen([sys.executable] + sys.argv)
-#                sys.exit()
-#
-#
-#
-#
-#
-#       THESE CODES ARE FOR A FUTURE UPDATE
-#        THEY WILL BE LIVE ONCE THE PROJECT GETS OUT OF PRE-PRELEASE.
-#
-#
-#
+if chooseOption == 2:
+   settingsMenu = """
+===============SETTINGS===============
+1 = Turn auto-update on/off
+"""
+   print(settingsMenu)
+   chooseSetting = int(input("Which option would you like to choose(1)?: "))
+   if chooseSetting == 1:
+       aUpdateMenu = """
+   ===============AUTO-UPDATE===============
+   1 = Turn on
+   2 = Turn off
+   """
+       print(aUpdateMenu)
+       aUpdateOption = input(
+           "Which option would you like to choose(1/2)?: ")
+       if aUpdateOption.lower() == "back":
+           subprocess.Popen([sys.executable] + sys.argv)
+           sys.exit()
+       if aUpdateOption == "1":
+           with open("auto_update.conf", "wb") as auto_update_configFile:
+               auto_update_configFile.write("True".encode())
+               print("Changes saved successfully!")
+               input("Press any key to restart...")
+               subprocess.Popen([sys.executable] + sys.argv)
+               sys.exit()
+       if aUpdateOption == "2":
+           with open("auto_update.conf", "wb") as auto_update_configFile:
+               auto_update_configFile.write("False".encode())
+               print("Changes saved successfully!")
+               input("Press any key to restart...")
+               subprocess.Popen([sys.executable] + sys.argv)
+               sys.exit()
